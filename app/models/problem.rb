bd26e5e67af213fa4121a6bab294405f98889102
Davida Marion <davidamarion@gmail.com>
2014-02-04 11:52:59 -0800

    scheduler task seems to be working

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -21,7 +21,7 @@ class Problem < ActiveRecord::Base
     @room.play 'trombone'
   end
 
-  def self.get_mad(problem)
+  def self.check_in(problem)
     find_room
     @room.speak problem
     @room.play 'crickets'
@@ -33,12 +33,11 @@ class Problem < ActiveRecord::Base
 
   def self.reminder
    where(helped: 'needs help').each do |problem|
-    # Having trouble accessing 'created_at' figure out why and how to fix.
-      if (Time.now - problem.created_at) > 600
-        puts 'this works'
-      else
-        puts "ten minutes is a long time"
-        # get_mad("#{Adie.find(problem.adie_id).name} STILL needs help. Can someone check it out, already? -- http://helplist.herokuapp.com/problems")
+    seconds = (Time.now - problem.created_at)
+      if seconds > 600 && seconds < 1200
+        check_in("#{Adie.find(problem.adie_id).name} STILL needs help. Can someone check it out, already? -- http://helplist.herokuapp.com/problems")
+      elsif seconds > 1200
+        check_in("Ahem. #{Adie.find(problem.adie_id).name} is STILL waiting. Get ON this -- http://helplist.herokuapp.com/problems")
       end
     end
   end
